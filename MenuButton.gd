d753998b274efcb2039720e779961ba76c588208
komdog <komdogart@gmail.com>
2022-09-05 21:53:22 -0400

    Optimized menu buttons

--- MenuButton.gd
+++ MenuButton.gd
@@ -1,9 +1,16 @@
 extends ColorRect
 
-onready var menu = $MenuTab
+onready var menus = $Tabs.get_children()
+onready var buttons = $Buttons.get_children()
 
+enum {PLAYER, SKILL, OPTIONS, MENU}
 
-func on_ready():
-	pass
+func _ready():
+	for i in buttons.size():
+		buttons[i].connect("button_up", self, "_on_button_pressed", [i])
 
-
+	
+func _on_button_pressed(idx: int):
+	for i in menus.size():
+		menus[i].visible = false
+	menus[idx].visible = true
