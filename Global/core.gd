32f8841a4fd48eb49afe4a4ce53c39b47ccdfe5b
komdog <komdogart@gmail.com>
2022-10-21 23:36:45 -0400

    Updated Core

--- Global/core.gd
+++ Global/core.gd
@@ -1,18 +1,51 @@
 extends Node
 
-var enemy_count : int
+signal update_hud()
+
+onready var enemy_timer = Timer.new()
+
+var enemy_index # The enemy that's supposed to do something
 var active_enemies = {
 	"gobokin": [],
 	"gangman": [],
 }
 
-func check_enemy_count() -> int:
-  # Make an empty array
+func _ready():
+	if Ply.connect('action_done', self, '_on_action_done') == OK: pass	
+	if enemy_timer.connect('timeout', self, '_on_timer_timeout') == OK: pass	
+
+	add_child(enemy_timer)
+
+func get_all_enemies() -> Array:
 	var all_enemies = []
-	
-  # Combine the arrays
+	# Combine the arrays
 	for array in active_enemies.values():
 		all_enemies.append_array(array)
 	
-	print(all_enemies)
-	return all_enemies.size()
+	return all_enemies
+
+func _on_action_done() -> void:
+	Terminal.disable()
+	# Start from the first enemy
+	enemy_index = 0
+
+	# Start timer
+	enemy_timer.wait_time = 1.0
+	enemy_timer.start()
+
+
+func _on_timer_timeout():
+	# Stop timer if all enemies are done doing stuff
+	if enemy_index >= get_all_enemies().size():
+		enemy_timer.stop()
+		Actions.emit_signal("new_turn")
+		Terminal.enable()
+		emit_signal("update_hud")
+		return 
+
+	# Make the next enemy do a thing
+	get_all_enemies()[enemy_index].run_anim()
+	enemy_index += 1
+
+	emit_signal("update_hud")
+		
--- Hud.gd
+++ Hud.gd
@@ -1,15 +1,18 @@
 extends Control
 
-
+onready var tween = Tween.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	if Actions.connect('new_turn', self, '_on_new_turn') == OK:
-		pass
+	if Core.connect('update_hud', self, '_on_update_hud') == OK: pass
+
+	add_child(tween)
 
 
-func _on_new_turn():
-	$PlayerInfo/HealthBar.value = Ply.health
+func _on_update_hud():
+	# $PlayerInfo/HealthBar.value = Ply.health
 	$PlayerInfo/HealthBar/PlayerHealth.text = "Health : %d/%d" % [Ply.health, Ply.max_health]
+	tween.interpolate_property($PlayerInfo/HealthBar, 'value', $PlayerInfo/HealthBar.value, Ply.health, 0.5, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	tween.start()
+	print("Health : %d/%d" % [Ply.health, Ply.max_health])
