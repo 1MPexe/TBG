7cc115a46e1000b7f9fc286ff6da86e79868a5e6
komdog <komdogart@gmail.com>
2022-10-27 20:09:48 -0400

    Added `percet_chance` functuons

--- Global/Utility.gd
+++ Global/Utility.gd
@@ -1,9 +1,10 @@
 extends Node
 
 
-
-
-
+# Rolls The Dice... Outputs true based on a percent chance you specify
+func percent_chance(percent_chance : int) -> bool:
+	randomize()
+	return (randi() % 100 < percent_chance)
 
 func random_number(max_num : int):
 	randomize()
